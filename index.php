848bd6e383bd7ed42dce69d86780e5e25e8c09f1
Claudio Alonso <cealonso@gmail.com>
2016-07-12 16:07:18 -0300

    Primer Commit para subida de archivos...

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<title>Restaurante</title>
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<img src="images/logo.gif" class="logo">
+            <!--  menú de navegación -->
+		</header>
+
+		<section id="main">
+			<!--  Codigo PHP -->
+		</section>
+		<footer>
+			<p>
+				&copy; 2016 <span id="destacado">Todos los derechos reservados</span>
+			</p>
+		</footer>
+	</div>
+</body>
+</html>
