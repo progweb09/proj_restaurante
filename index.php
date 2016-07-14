bc8b9e86dc0a24a8e2ecae25be5f7a6b68b33665
julindres <tengo3cmdepito@gmail.com>
2016-07-14 20:27:53 -0300

    asdjgfihadgsfiagh

--- index.php
+++ index.php
@@ -9,9 +9,6 @@
 	rel='stylesheet' type='text/css'>
 <meta charset="UTF-8" />
 <title>Restaurante</title>
-<link href="css/main.css" type="text/css" rel="stylesheet">
-<link href="css/sweetalert.css" type="text/css" rel="stylesheet">
-<script src="js/sweetalert.min.js"></script>
 </head>
 <body>
 	<div id="wrapper">
@@ -19,27 +16,16 @@
 			<img src="images/logo.gif" class="logo">
 			<nav>
 				<ul>
-<<<<<<< HEAD
 					<li><a href="">HOME</a></li>
 					<li><a href="">CARTA</a></li>
 					<li><a href="" id="linkPromo">PROMOCIONES</a></li>
 					<li><a href="">CONTACTENOS</a></li>
-=======
-					<li><a href="#" title="Home">Home</a></li>
-					<li><a href="#" title="Carta">Carta</a></li>
-					<li><a href="#" title="Promociones" id="lnk_promociones">Promociones</a></li>
-					<li><a href="#" title="Contactenos">Contactenos</a></li>
->>>>>>> origin/master
 				</ul>
 			</nav>
 		</header>
 
 		<section id="main">
-<<<<<<< HEAD
 			<?php
-=======
-		<?php 
->>>>>>> origin/master
 			$platos = array (
 					"carne.jpg",
 					"ensaladas.jpg",
@@ -48,10 +34,6 @@
 					"pizza.jpg",
 					"tarta.jpg" 
 			);
-<<<<<<< HEAD
-=======
-			
->>>>>>> origin/master
 			$precio = array (
 					"150 PESOS",
 					"120 PESOS",
@@ -60,7 +42,6 @@
 					"100 PESOS",
 					"115 PESOS" 
 			);
-<<<<<<< HEAD
 			$diaActual = date ( 'w' );
 			echo "<h1 style='text-align: center; color:#e20018'>MENÚ DEL DÍA</h1>";
 			
@@ -70,29 +51,6 @@
 			<div class="precio"><?php echo $precio[$diaActual]?></div>
 			<br />
 	<div id="servicios"></div>
-=======
-			
-			$diaActual = date ('w');
-			
-			echo "<h1 style='text-align:center;color:#e20018'> MENÚ DEL DIA </h1>";
-						
-		?>
-		
-		   <img style="display: block; margin: 0px auto"
-				src="images/<?php echo $platos[$diaActual] ?>" alt="foto del plato">
-				
-		   <br>
-		   
-		<?php
-			echo "<h1 style='display: block; margin: 0px auto; width:20%; 
-			text-align:center;color:#ffffff;background-color:#000000'> 
-			$precio[$diaActual] </h1>";
-		?>
-			
-		   
-		   
-		   
->>>>>>> origin/master
 		</section>
 		
 		<footer>
@@ -101,7 +59,6 @@
 			</p>
 		</footer>
 	</div>
-<<<<<<< HEAD
 	<script>
 	$(document).ready(function(){
 		$.getJSON("servicios.json",function(datos){
@@ -111,7 +68,7 @@
 			}
 			});
 			});
-
+ 
 	</script>
 
 	<script>
@@ -120,22 +77,5 @@
 }
 			linkPromo.addEventListener('click', mostrarPromocion);
 	</script>
-=======
-	
-	<script>
-
-    function mostrarPromocion(){
-
-    	sweetAlert('¡ Promoción !', 'Abonando en Efectivo 10% de Descuento.');	
-
-        
-    }
-
-    lnk_promociones.addEventListener('click', mostrarPromocion);
-
-	</script>
-	
-	
->>>>>>> origin/master
 </body>
 </html>
